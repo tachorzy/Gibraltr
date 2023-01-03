b86b2b08388c4f8eb178e886c1ee49a52c3fa5b8
Tariq Achor Zyad <tachorzyad@gmail.com>
2023-01-02 21:08:57 -0600

    Filtered Non-Countries and Simplified Country Names

--- app/components/searchbar.js
+++ app/components/searchbar.js
@@ -6,10 +6,12 @@ import { useEffect, useState } from 'react';
 import { getCodeList, overwrite } from 'country-list'
 
 const SearchBar = () => {
-    const nonCountries = ['aq', 'as', 'ai', 'aw', 'ax', 'bm', 'bl', 'bq', 'bv', 'io', 'ky', 'cx', 'cc', 'ck', 'fk', 'fo', 'gf', 
-    'pf', 'tf', 'gi', 'gl', 'gp', 'gu', 'hm', 'mq', 'yt', 'ms', 'an', 'nc', 'nu', 'nf', 'mp', 'pn', 'pr', 're',
-     'sh', 'pm', 'sj', 'tk', 'tc', 'um', 'vg', 'wf', 'eh'];
-        
+    //removing all non-countries from the listing, e.g. Antarctica and British/French/Dutch overseas territories 
+    const nonCountries = ['aq', 'as', 'ai', 'aw', 'ax', 'bm', 'bl', 'bq', 'bv', 'cw', 'io', 'ky', 'cx', 'cc', 'ck', 'fk', 'fo', 'gf', 
+    'pf', 'tf', 'gg' , 'gi', 'gl', 'gp', 'gs', 'gu', 'hm', 'im', 'je', 'mf', 'mq', 'yt', 'ms', 'an', 'nc', 'nu', 'nf', 'mp', 'pn', 'pr', 're',
+     'sh', 'pm', 'sj', 'sx', 'tk', 'tc', 'um', 'vg', 'vi', 'wf', 'eh'];
+    
+    //overwriting all official names to their more commonly known names, as well as changing some endonyms to their English exonyms
     overwrite([
         {
             code: 'GB',
@@ -34,10 +36,66 @@ const SearchBar = () => {
         {
             code: 'CD',
             name: 'Democratic Rep. of the Congo'
+        },
+        {
+            code: 'IR',
+            name: 'Iran'
+        },
+        {
+            code: 'MO',
+            name: 'Macau'
+        },
+        {
+            code: 'VN',
+            name: 'Vietnam'
+        },
+        {
+            code: 'TW',
+            name: 'Taiwan'
+        },
+        {
+            code: 'CI',
+            name: 'Ivory Coast'
+        },
+        {
+            code: 'KP',
+            name: 'North Korea'
+        },
+        {
+            code: 'KR',
+            name: 'South Korea'
+        },
+        {
+            code: 'LA',
+            name: 'Laos'
+        },
+        {
+            code: 'MD',
+            name: 'Moldova'
+        },
+        {
+            code: 'RU',
+            name: 'Russia'
+        },
+        {
+            code: 'SY',
+            name: 'Syria'
+        },
+        {
+            code: 'TZ',
+            name: 'Tanzania'
+        },
+        {
+            code: 'VA',
+            name: 'Vatican City'
+        },
+        {
+            code: 'VE',
+            name: 'Venezuela'
         }
     ])
+    //retrieving the list of the countries, first as an Object with keys and values then we filter that list of the keys of the non-countries
     const countries = getCodeList()
-    const countryCodeObject = Object.keys(countries)
     const countryCodeList = Object.keys(countries).filter(code => 
         !nonCountries.includes(code)
         )
