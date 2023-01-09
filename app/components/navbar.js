b1a09613b2257e810070b19b2fbb3cd0b147b788
Tariq Achor Zyad <tachorzyad@gmail.com>
2023-01-09 04:39:01 -0600

    Added custom font for branding name

--- app/components/navbar.js
+++ app/components/navbar.js
@@ -1,16 +1,22 @@
 import NavStyle from '../styles/NavBarStyle.module.css'
 import Image from 'next/image'
 import { Roboto, Montserrat, Rubik } from '@next/font/google'
+import localFont from '@next/font/local'
 
 const rubik = Rubik({
     subsets: ['latin'],
     weight: '400'
 })
 
+const albula = localFont({
+    src: '../fonts/serpentype-albula-pro-bold.otf',
+    weight: '500'
+})
+
 const NavBar = () => {
 
     return(
-        <div className={rubik.className}>
+        <div className={albula.className}>
             <div className={NavStyle.navContainer}>
                 <Image src="Gibraltar.svg" width={90} height={90} className={NavStyle.logo}></Image>
                 <h1 className={NavStyle.brandName}>Gibraltr</h1>
