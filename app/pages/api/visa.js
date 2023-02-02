af5fab542720b3cb93d5ab20ce9bc94d5164484a
Tariq Achor Zyad <tachorzyad@gmail.com>
2023-02-01 22:59:54 -0600

    Removed old unused code in result.js

---
+++ app/pages/api/visa.js
@@ -0,0 +1,21 @@
+import fetch from 'node-fetch';
+import { getVisaRequirements } from '../../scripts/visadata.js'
+
+export default async function handler(searchPassport, searchDestination) {
+
+    try {
+        const visaRequirements = await getVisaRequirements();
+        const key = JSON.stringify({ searchPassport, searchDestination })
+        const requirement = visaRequirements.get(key);
+
+        if (requirement) {
+            res.json({ Requirement: requirement });
+        } else {
+            res.status(404).json({ message: `No match found for ${searchPassport} ${searchDestination}` });
+        }
+ 
+    } catch (error) {
+        console.error(error);
+        // res.status(500).json({ message: 'An error occurred' });
+    }
+}
