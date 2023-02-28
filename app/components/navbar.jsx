c9d7a345c3c3720d7d35694b4ae0c17c28434745
Tariq Achor Zyad <tachorzyad@gmail.com>
2023-02-27 22:47:33 -0600

    Added links to navbar

--- app/components/navbar.jsx
+++ app/components/navbar.jsx
@@ -1,5 +1,6 @@
 import NavStyle from '../styles/NavBarStyle.module.css'
 import Image from 'next/image'
+import Link from 'next/link'
 import { Roboto, Montserrat, Rubik } from '@next/font/google'
 import localFont from '@next/font/local'
 
@@ -18,9 +19,9 @@ const NavBar = () => {
                     <h1 className={NavStyle.brandName}>Gibraltr</h1>
                 </div>
                 <ul className={NavStyle.navItems}>
-                    <li className={NavStyle.navItem}>Find Visas</li>
-                    <li className={NavStyle.navItem}>About</li>
-                    <li className={NavStyle.navItem}>FAQ</li>
+                    <Link className={NavStyle.navItem} href="/">Find Visas</Link>
+                    <Link className={NavStyle.navItem} href="/about">About</Link>
+                    <Link className={NavStyle.navItem} href="/faq">FAQ</Link>
                 </ul>
             </div>
         </div>
