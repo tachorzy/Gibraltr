dfba2bb89f94d857026b45972168576a46e4cc01
Tariq Achor Zyad <tachorzyad@gmail.com>
2023-03-05 15:19:42 -0600

    Added TravelBan component to pages with banned travel

--- app/pages/destinations/[destination].jsx
+++ app/pages/destinations/[destination].jsx
@@ -63,12 +63,49 @@ function displayVisaMessage(passport, destination, requirement) {
 }
 
 function displayTravelBan(passport, destination, requirement){
-    if(passport === "Pakistan" && destination === "Israel")
+    if (passport === "Israel")
+        switch(destination) {
+            case "Algeria":
+                return(<TravelBan></TravelBan>)
+            case "Brunei":
+                return(<TravelBan></TravelBan>)
+            case "Iran":
+                return(<TravelBan></TravelBan>)
+            case "Iraq":
+                return(<TravelBan></TravelBan>)
+            case "Kuwait": 
+                return(<TravelBan></TravelBan>)
+            case "Lebanon":
+                return(<TravelBan></TravelBan>)
+            case "Libya":
+                return(<TravelBan></TravelBan>)
+            case "Malaysia":
+                return(<TravelBan></TravelBan>)
+            case "Pakistan":
+                return(<TravelBan></TravelBan>)
+            case "Saudi Arabia":
+                return(<TravelBan></TravelBan>)
+            case "Syria":
+                return(<TravelBan></TravelBan>)
+            case "Yemen":
+                return(<TravelBan></TravelBan>)
+        }
+
+    if (passport == "Iran" && destination === "Libya")
         return(<TravelBan></TravelBan>)
+    if (passport === "North Korea" && destination === "Japan")
+        return(<TravelBan></TravelBan>)
+    if (passport === "Pakistan" && destination === "Israel")
+        return(<TravelBan></TravelBan>)
+    if (passport === "United States" && destination === "North Korea")
+        return(<TravelBan></TravelBan>)
+    /*Restriction is unclear for Ukranians entering Russia, Passport Index lists it as visa free but other sources site it as restricted*/
+    // if (passport === "Ukraine" && destination === "Russia")
+    //     return(<TravelBan></TravelBan>)
 }
 
 function displayVisaInfo(requirement){
-    switch(requirement){
+    switch (requirement) {
         case "e-visa":
             return(<ElectronicVisa/>)
         case "visa required":
@@ -82,7 +119,7 @@ function displayVisaInfo(requirement){
 
 function displaySchengenInfo(destination){
     let destinationISO = isoCodesList[destination.toLowerCase()].toLowerCase()
-    if(schengenCountries.includes(destinationISO))
+    if (schengenCountries.includes(destinationISO))
         return (<Schengen></Schengen>)
 }
 
