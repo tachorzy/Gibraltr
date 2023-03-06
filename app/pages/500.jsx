5bf807e7db5502957983e4ce09e79cf53d70d691
Tariq Achor Zyad <tachorzyad@gmail.com>
2023-03-05 21:19:21 -0600

    Created 500.jsx page based on 400.jsx code

---
+++ app/pages/500.jsx
@@ -0,0 +1,50 @@
+import Head from 'next/head'
+import Image from 'next/image'
+import NavBar from '../components/navbar'
+import SearchBar from '../components/searchbar'
+import HomePageInfo from '../components/homepageInfo'
+import InfoCard from '../components/infocard'
+import Footer from '../components/footer'
+import { Combo, Roboto, Rubik } from '@next/font/google'
+import localFont from '@next/font/local'
+import styles from '../styles/Home.module.css'
+import AboutStyles from '../styles/AboutUsStyle.module.css'
+import ErrorStyles from '../styles/ErrorStyle.module.css'
+import "animate.css"
+
+
+const roboto = Roboto({ 
+  subsets: ['latin'], 
+  weight: '400' 
+})
+
+const supremeMedium = localFont({
+  src: '../fonts/Supreme-Medium.otf',
+  weight: '200'
+})
+
+export default function ServerError() {
+  return (
+    <>  
+      <Head>
+        <title>Page Not Found</title>
+        <meta name="description" content="It's Best to Know Before You Go!" />
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
+        <link rel="icon" href="/gibraltar-solid.svg" />
+      </Head>
+
+      <main>
+          <div className={styles.navBar}>
+              <NavBar></NavBar>
+          </div>
+          <div className={supremeMedium.className}>
+            <div className={ErrorStyles.bannerHeader}>
+                <h2 className={ErrorStyles.title}>Papers Please! You've ventured a page too far.</h2>
+            </div>
+          </div>
+          <div></div>
+          <Footer></Footer>
+      </main>
+    </>
+  )
+}
