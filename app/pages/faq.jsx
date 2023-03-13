4d3346281fa8cabc950e8d4ddaf85d116fd043cd
Tariq Achor Zyad <tachorzyad@gmail.com>
2023-03-13 03:53:51 -0500

    Converted static FAQ page text into a hashmap

--- app/pages/faq.jsx
+++ app/pages/faq.jsx
@@ -1,15 +1,13 @@
 import Head from 'next/head'
 import Image from 'next/image'
 import NavBar from '../components/navbar'
-import SearchBar from '../components/searchbar'
-import HomePageInfo from '../components/homepageInfo'
-import InfoCard from '../components/infocard'
 import Footer from '../components/footer'
 import { Combo, Roboto, Rubik } from '@next/font/google'
 import localFont from '@next/font/local'
 import styles from '../styles/Home.module.css'
 import AboutStyles from '../styles/AboutUsStyle.module.css'
 import FAQStyles from '../styles/FAQStyle.module.css'
+import { visaFAQContent, generalFAQContent } from '../utils/faqdata.jsx'
 import "animate.css"
 
 
@@ -27,6 +25,44 @@ const rubik = Rubik({
   subsets: ['latin'],
   weight: '400'
 })
+
+function VisaFAQ(){
+  return (
+    <div>
+        {Array.from(visaFAQContent.entries()).map(([visaFAQQuestion, visaFAQParagraph]) => {
+          return(
+          <details>
+            <summary className={FAQStyles.detailsHeader}>
+            {visaFAQQuestion}
+            </summary>
+            <div className={FAQStyles.detailsText}>
+              {visaFAQParagraph}
+            </div>
+          </details>
+          )
+        })}
+    </div>
+  );
+}
+
+function GeneralFAQ(){
+  return (
+    <div>
+        {Array.from(generalFAQContent.entries()).map(([generalFAQQuestion, generalFAQParagraph]) => {
+          return(
+          <details>
+            <summary className={FAQStyles.detailsHeader}>
+            {generalFAQQuestion}
+            </summary>
+            <div className={FAQStyles.detailsText}>
+              {generalFAQParagraph}
+            </div>
+          </details>
+          )
+        })}
+    </div>
+  );
+}
 
 export default function Home() {
   return (
@@ -49,70 +85,12 @@ export default function Home() {
             
             <section className={FAQStyles.section}>
               <h2 className={FAQStyles.subtitle}>Visa FAQ</h2>
-              <details>
-                <summary className={FAQStyles.detailsHeader}>
-                What is a travel visa?
-                </summary>
-                <p className={FAQStyles.detailsText}>A travel visa is a document that grants you the ability to enter a country via an authorized port of entry and to stay in that country for a specific length of time. There are different types of visas that serve different purposes. Some are for students, residents, spouses, and tourists. Our search engine connects you with the latest information for <b>tourist visas</b>.</p>
-              </details>
-              <details>
-                <summary className={FAQStyles.detailsHeader}>
-                Why do you need a visa?
-                </summary>
-                <p className={FAQStyles.detailsText}>A visa is what allows you entry into another country. Without one, you may be denied entry into your destination. That's why it is crucial to understand the visa requirements of your destination before you book that next trip. </p>
-              </details>
-              <details>
-                <summary className={FAQStyles.detailsHeader}>
-                What are the different types of visa requirements?
-                </summary>
-                <p className={FAQStyles.detailsText}>Different countries will demand different requirements based on an traveler's nationality. Some countries may not require you to get a visa at all, in this case they are <b>visa free</b>. Some may require an electronic visa commonly referred to as an <b>e-Visa</b>. Depending on the country, these may also be referred to as electronic travel authorizations. There are also countries that provide a <b>visa on arrival</b> to certain nationalities. In this case, a traveler arrives to their destination's port of entry and purchases a visa. For most of the world however, none of these options are available. No matter your passport, you'll most likely notice some countries that require you to get an <b>embassy visa</b>. In these cases, travelers must visit an embassy or consulate in order to apply for a visa in person.</p>
-              </details>
-              <details>
-                <summary className={FAQStyles.detailsHeader}>
-                How do you apply for an e-Visa?
-                </summary>
-                <p className={FAQStyles.detailsText}>An electronic visa, or e-Visa, is an digital alternative to a traditional visa. These are filled out online and ahead of arrival. Generally, travelers apply for their e-Visa through an official website for their destination or through a third party travel agency. These applications involve providing a passport photo, personal information and basic details about your planned visit to the country.</p>
-              </details>
-              <details>
-                <summary className={FAQStyles.detailsHeader}>
-                What is a Schegen visa?
-                </summary>
-                <p className={FAQStyles.detailsText}>The growing cooperation of European nations has resulted in the success of the Schengen Area — an area encompassing 27 European countries that share open borders. Upon entry into the Schengen Area, your visa to a Schengen member country will also count as a Schengen visa. With this visa, travelers are granted a stay of up to 90 days in a 180-day period, and may freely move within the Schengen area.</p>
-              </details>
+              <VisaFAQ></VisaFAQ>    
             </section>
 
             <section className={FAQStyles.section}>
               <h2 className={FAQStyles.subtitle}>General FAQ</h2>
-              <details>
-                <summary className={FAQStyles.detailsHeader}>
-                How many countries are available on Gibraltr?
-                </summary>
-                <p className={FAQStyles.detailsText}>Gibraltr provides the latest information on visa requirements for <b>199</b> nationalities and destinations.</p>
-              </details>
-              <details>
-                <summary className={FAQStyles.detailsHeader}>
-                Do you plan on supporting more nationalities?
-                </summary>
-                <p className={FAQStyles.detailsText}>It's our mission to represent and include as many nations as we can through available data. However, it's not an easy task to present every nation in the world. Some countries simply slip through the cracks and end up being left out, one such country is the partially recognized nation of Kosovo. Nevertheless, we do plan to add every nation with a well-founded diplomacy, visa policy and passport.</p>
-              </details>
-              <details>
-                <summary className={FAQStyles.detailsHeader}>
-                Where are you sourcing your data?
-                </summary>
-                <p className={FAQStyles.detailsText}>We receive our information on visa requirements through a regularly updated open-source data set built off of the Passport Index API.</p>
-              </details>
-              <details>
-                <summary className={FAQStyles.detailsHeader}>
-                Are your results a reliable source?
-                </summary>
-                <p className={FAQStyles.detailsText}>Our results should always reflect the latest data in the Passport Index. Gibraltr is a gateway to the latest visa information accessible to us. The rest of the work lies in your hands as a responsible visa applicant and traveler. Along the way in your research you'll form a sound and sensible itinerary.</p>
-              </details>
-              {/* <details>
-                <summary className={FAQStyles.detailsHeader}>
-                What's a Schegen visa?
-                </summary>
-                <p className={FAQStyles.detailsText}> test test</p>
-              </details> */}
+              <GeneralFAQ></GeneralFAQ>
             </section>
           
           </div>
