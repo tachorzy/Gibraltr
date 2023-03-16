9474cce960bd9be1bd61c37f1d6e9376bd8eba61
Tariq Achor Zyad <tachorzyad@gmail.com>
2023-03-16 13:40:17 -0500

    Refactored about.jsx, introduced section and span tags

--- app/pages/about.jsx
+++ app/pages/about.jsx
@@ -45,8 +45,8 @@ export default function AboutUs() {
             </div>
           </div>
 
-          <div className={AboutStyles.main}>
-            <div className={supremeMedium.className}>
+          <section className={AboutStyles.main}>
+            <span className={supremeMedium.className}>
                 <h2 className={AboutStyles.aboutUsTitle}>About us</h2>
                 <p className={AboutStyles.paragraphLast}>
                   {"Gibraltr was founded with the initiative of making travel visas easier to understand. In our everchanging connected world, travelers are responsible for knowing the power & privilege of their passport. And, you don't have to do it alone! Our search engine provides accurate and reliable visa information for all passport holders no matter their destination."}
@@ -54,50 +54,51 @@ export default function AboutUs() {
                 {/* <h2 className={AboutStyles.subtitle}>Travel at ease</h2>
                 <h3 className={AboutStyles.subtitle}>Wisely plan your trip</h3>
                 <h4 className={AboutStyles.centeredTitle}>Our mission is guiding you through the complexities of tourist visas.</h4> */}
-            </div>
-          </div>
+            </span>
+          </section>
           <div className={AboutStyles.bannerLight}>
             <div className={styles.infoCard}>
-              <div className={supremeMedium.className}>
+              <span className={supremeMedium.className}>
                 <h2 className={AboutStyles.infoCardTitle}>197+<br/>countries
                 </h2>
                 <p className={AboutStyles.infoCardText}>Representing every destination.</p>
-              </div>
+              </span>
             </div>
 
             <div className={styles.infoCard}>
-              <div className={supremeMedium.className}>
+              <span className={supremeMedium.className}>
                 <h2 className={AboutStyles.infoCardTitle}>197+<br/>passports
                 </h2>
                 <p className={AboutStyles.infoCardText}>Available for every nationality.</p>
-              </div>
+              </span>
             </div>
 
             <div className={styles.infoCard}>
-              <div className={supremeMedium.className}>
+              <span className={supremeMedium.className}>
                 <h2 className={AboutStyles.infoCardTitle}>Over 3000<br/>embassies
                 </h2>
                 <p className={AboutStyles.infoCardText}>Accesible to all travelers.</p>
-              </div>
+              </span>
             </div>
 
             <div className={styles.infoCard}>
-              <div className={supremeMedium.className}>
+              <span className={supremeMedium.className}>
                 <h2 className={AboutStyles.infoCardTitle}>Endless<br/>results
                 </h2>
                 <p className={AboutStyles.infoCardText}>Remaining reliable and up to date.</p>
-              </div>
+              </span>
             </div>
 
           </div>
 
-          <div className={AboutStyles.main}>
-            <div className={supremeMedium.className}>
+          <section className={AboutStyles.main}>
+            <span className={supremeMedium.className}>
               <h4 className={AboutStyles.aboutUsTitle} id="our-vision">Our vision</h4>
               <p className={AboutStyles.paragraphLast}>{"It's our mission to help you better understand your passport. Our passports are a privilege, each of them with their own freedoms and limits. Entering countries is not easy for all of us. Whether you have a strong or weak passport will greatly shape your travels. We aim to empower all of those yearning to travel. No matter their nation of origin."} </p>
               {/* <p className={AboutStyles.paragraphLast}>We want to simplify the process of finding visa requirements.</p> */}
-            </div>
-          </div>
+                
+            </span>
+          </section>
           <Footer></Footer>
       </main>
     </>
