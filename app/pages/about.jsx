fdf903d1eda3776a51d12d4ce99168573dcee189
Tariq Achor Zyad <tachorzyad@gmail.com>
2023-03-17 02:52:48 -0500

    Added developer story to about.jsx

--- app/pages/about.jsx
+++ app/pages/about.jsx
@@ -3,6 +3,7 @@ import Image from 'next/image'
 import NavBar from '../components/navbar'
 import InfoCard from '../components/infocard'
 import Footer from '../components/footer'
+import { TypeAnimation } from 'react-type-animation'
 import { Combo, Roboto, Rubik } from '@next/font/google'
 import localFont from '@next/font/local'
 import styles from '../styles/Home.module.css'
@@ -96,7 +97,56 @@ export default function AboutUs() {
               <h4 className={AboutStyles.aboutUsTitle} id="our-vision">Our vision</h4>
               <p className={AboutStyles.paragraphLast}>{"It's our mission to help you better understand your passport. Our passports are a privilege, each of them with their own freedoms and limits. Entering countries is not easy for all of us. Whether you have a strong or weak passport will greatly shape your travels. We aim to empower all of those yearning to travel. No matter their nation of origin."} </p>
               {/* <p className={AboutStyles.paragraphLast}>We want to simplify the process of finding visa requirements.</p> */}
-                
+              <div className={AboutStyles.infoContainerSplit}>
+                <div>
+                  <TypeAnimation
+                    className={AboutStyles.welcomeMessage}
+                    sequence={[
+                      'Hello',
+                      3000,
+                      "Hola",
+                      3000,
+                      "Hej",
+                      3000,
+                      'Bonjour',
+                      3000,
+                      'Olá',
+                      3000,
+                      'Привет',
+                      3000,
+                      'Γειά σου',
+                      3000,
+                      'Merhaba',
+                      3000,
+                      'سلام',
+                      3000,
+                      '환영',
+                      3000,
+                      '你好',
+                      3000,
+                      'नमस्ते',
+                      3000,
+                      'আরে',
+                      3000,
+                      'สวัสดี',
+                      3000,
+                    ]} 
+                    repeat={Infinity}
+                    speed={25}
+                    deletionSpeed={25}
+                  />
+                  <h3 className={AboutStyles.developerName}>from Tariq Achor Zyad</h3>
+                  <h3 className={AboutStyles.developerCredit}>Founder and sole developer of Gibraltr</h3>
+                </div>
+                <div className={AboutStyles.developerStory}>                  
+                  <h4 className={AboutStyles.aboutUsTitle}>Made by a traveler for travelers</h4>
+                  <p className={AboutStyles.paragraphRegular}><strong>{"Planning a trip doesn't come without copious amounts of research."} </strong> {"That research begins with finding out whether or not you'll need a visa. Often times the struggle for a visa may shatter your dreams. Or the search for the right e-Visa application might just be a headache. That's why when I started Gibraltr, I wanted to create a candid tool for travelers like myself to better get a hold of tourist visas."}</p>
+                  <p className={AboutStyles.paragraphLast}>{""}</p>
+                </div>
+              </div>
+
+              <h4 className={AboutStyles.aboutUsTitle}>An Open Source Solution for Visas</h4>
+              <p className={AboutStyles.paragraphLast}>{"LOREM IPSUM LOREM IPSUM LOREM IPSUM"}</p>
             </span>
           </section>
           <Footer></Footer>
