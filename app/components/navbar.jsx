69768e841c25275e563ac2656c6507398f4e1214
Tariq Achor Zyad <tachorzyad@gmail.com>
2023-04-17 15:35:44 -0500

    Added hamburger menu for mobile

--- app/components/navbar.jsx
+++ app/components/navbar.jsx
@@ -3,6 +3,7 @@ import Image from 'next/image'
 import Link from 'next/link'
 import { Roboto, Montserrat, Rubik } from '@next/font/google'
 import localFont from '@next/font/local'
+import HamburgerMenu from './hamburgerMenu'
 
 const albula = localFont({
     src: '../fonts/serpentype-albula-pro-bold.otf',
@@ -15,11 +16,14 @@ const NavBar = () => {
         <div className={albula.className}>
             <div className={NavStyle.navContainer}>
                 <div className={NavStyle.brandingContainer}>
-                    <Image src="/Gibraltar.svg" width={60} height={60} className={NavStyle.logoMobile}></Image>
+                    <Image src="/Gibraltar.svg" width={75} height={75} className={NavStyle.logoMobile}></Image>
                     
                     <Image src="/Gibraltar.svg" width={90} height={90} className={NavStyle.logoDesktop}></Image>
                 
                     <h1 className={NavStyle.brandName}>Gibraltr</h1>
+                </div>
+                <div className={NavStyle.hamburgerMenu}>
+                    <HamburgerMenu></HamburgerMenu>
                 </div>
                 <ul className={NavStyle.navItems}>
                     <Link className={NavStyle.navItem} href="/">Find Visas</Link>
