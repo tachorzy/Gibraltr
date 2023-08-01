073de3add894d2129859d429ab8405e65c7c4c59
tachorzy <tachorzyad@gmail.com>
2023-08-01 02:06:35 -0500

    Added inline styling to EVisa and renamed it ElectronicVisa

---
+++ components/ElectronicVisa.jsx
@@ -0,0 +1,21 @@
+import styles from '../styles/Home.module.css'
+import localFont from '@next/font/local'
+
+const supremeMedium = localFont({
+    src: '../fonts/Supreme-Medium.otf',
+    weight: '200'
+  })
+
+const ElectronicVisa = () => {
+
+    return(
+        <div className={"w-96 h-40 text-stone-700 border-2 border-inherit border-solid rounded-xl p-4 pt-5 border-stone-300 bg-stone-200"}>
+            <span className={supremeMedium.className}>
+                <p className={"text-lg font-bold col-span-2 mb-2.5"}>e-Visa</p>
+                <p className={"leading-relaxed"}>Electronic visas are filled out online on an official government website or through a travel agency.</p>
+            </span>
+        </div>  
+    );
+}
+
+export default ElectronicVisa;
