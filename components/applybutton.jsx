3fc8eb2eb902b9b90450fa5fb94b242bd98f6599
tachorzy <tachorzyad@gmail.com>
2023-08-01 02:04:25 -0500

    Added inline styling to ApplyButton

--- components/applybutton.jsx
+++ components/applybutton.jsx
@@ -1,5 +1,3 @@
-import styles from '../styles/Home.module.css'
-import DestinationStyles from '../styles/DestinationStyle.module.css'
 import Image from 'next/image'
 import localFont from '@next/font/local'
 
@@ -11,9 +9,9 @@ const supremeMedium = localFont({
 const ApplyButton = ({title}) => {
     let evisa;
     return(
-            <button className={DestinationStyles.button}>
+            <button className={"w-96 h-16 bg-stone-600 hover:bg-stone-700 text-stone-200 font-semibold text-xl rounded-xl ring-stone-600 border-none cursor-pointer"}>
                 <div className={supremeMedium.className}>
-                    <div className={DestinationStyles.buttonLinkContainer}>
+                    <div className={"flex flex-row justify-center gap-x-4"}>
                         <p>Apply for an e-Visa!</p>
                             <Image src="/ApplyButtonLink.svg" width={20} height={20} className={DestinationStyles.buttonLinkIcon}></Image>
                     </div>
