0f34dbcbffacb7211707afc258410c04bae77fb5
tachorzy <tachorzyad@gmail.com>
2023-08-01 14:36:54 -0500

    Refactored ApplyButton and ElectronicVisa -- localFont in className

--- components/applybutton.jsx
+++ components/applybutton.jsx
@@ -10,11 +10,9 @@ const ApplyButton = ({title}) => {
     let evisa;
     return(
             <button className={"w-96 h-16 bg-stone-600 hover:bg-stone-700 text-stone-200 font-semibold text-xl rounded-xl ring-stone-600 border-none cursor-pointer"}>
-                <div className={supremeMedium.className}>
-                    <div className={"flex flex-row justify-center gap-x-4"}>
-                        <p>Apply for an e-Visa!</p>
-                            <Image src="/ApplyButtonLink.svg" width={20} height={20} className={DestinationStyles.buttonLinkIcon}></Image>
-                    </div>
+                <div className={supremeMedium.className + " flex flex-row justify-center gap-x-4"}>
+                    <p>Apply for an e-Visa!</p>
+                        <Image src="/ApplyButtonLink.svg" width={20} height={20} className={DestinationStyles.buttonLinkIcon}></Image>
                 </div>
             </button>  
     );
