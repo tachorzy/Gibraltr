43b3a450ec71b6b0541d339d5bf09702140202d0
tachorzy <tachorzyad@gmail.com>
2023-08-13 14:48:27 -0500

    Added inline styling to both IraqiKurdistanRegion and KETA

---
+++ components/IraqiKurdistanRegion.jsx
@@ -0,0 +1,24 @@
+import styles from '../styles/Home.module.css'
+import Image from 'next/image'
+import localFont from '@next/font/local'
+
+const supremeMedium = localFont({
+    src: '../fonts/Supreme-Medium.otf',
+    weight: '200'
+  })
+
+const IraqiKurdistanRegion = () => {
+
+    return(
+        <div className={supremeMedium.className + " w-96 h-40 text-stone-700 border-2 border-inherit border-solid rounded-xl p-4 pt-5 border-stone-300 bg-stone-200"}>
+            <span className={"grid grid-cols-3"}>
+                <p className={"text-lg font-bold col-span-2 mb-2.5 align-middle"}>Iraqi Kurdistan Region</p>
+                <Image src='/iraqborder.svg' width={35} height={35} className={"ml-16"}></Image>
+            </span>
+
+            <p className={"leading-relaxed"}>If you enter the country via Iraqi Kurdistan, you will be issued a visa that is only valid within the Kurdish Regional Government.</p>
+        </div>  
+    );
+}
+
+export default IraqiKurdistanRegion;
---
+++ components/KETA.jsx
@@ -0,0 +1,25 @@
+import styles from '../styles/Home.module.css'
+import Image from 'next/image'
+import localFont from '@next/font/local'
+
+const supremeMedium = localFont({
+    src: '../fonts/Supreme-Medium.otf',
+    weight: '200'
+  })
+
+const KETA = () => {
+
+    return(
+        <div className={supremeMedium.className + " w-96 h-40 text-stone-700 border-2 border-inherit border-solid rounded-xl p-4 pt-5 border-stone-300 bg-stone-200"}>
+            <span className={"grid grid-cols-3"}>
+                <p className={"text-lg font-bold col-span-2 mb-2.5 align-middle"}>K-ETA</p>
+                {/* <p className={styles.visaInfoBoxTitleSmall}>{"(Korea Electronic Travel Authorization)"}</p> */}
+                <Image src='/taeguk.svg' width={35} height={35} className={"ml-16"}></Image>
+            </span>
+
+            <p className={"leading-relaxed"}>The K-ETA is a travel authorization that visa-free visitors to South Korea must receive via an online application.</p>
+        </div>  
+    );
+}
+
+export default KETA;
