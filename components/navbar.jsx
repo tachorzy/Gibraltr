da027aaa840c9020209357a71d41627443442ffb
tachorzy <tachorzyad@gmail.com>
2023-08-13 04:44:17 -0500

    Changed NavBar to include inline styling

--- components/navbar.jsx
+++ components/navbar.jsx
@@ -10,34 +10,32 @@ const albula = localFont({
     weight: '500'
 })
 
+
+const navItems = new Map([
+    ['Find Visas', { link: '/'}],
+    ['About', { link: '/about'}],
+    ['FAQ', { link: '/faq'}],
+])
+
 const NavBar = () => {
-
-    const navItems = new Map([
-        ['Find Visas', { link: '/'}],
-        ['About', { link: '/about'}],
-        ['FAQ', { link: '/faq'}],
-    ])
     
-
     return(
-        <div className={albula.className}>
-            <div className={NavStyle.navContainer}>
-                <div className={NavStyle.brandingContainer}>
-                    <Image src="/Gibraltar.svg" width={75} height={75} className={NavStyle.logoMobile}></Image>
-                    
-                    <Image src="/Gibraltar.svg" width={90} height={90} className={NavStyle.logoDesktop}></Image>
+        <div className={albula.className + " fixed top-0 align-baseline	bg-stone-200 h-20 w-screen space-x-12 md:space-x-96 flex flex-row shadow-lg z-50"}>
+            <div className={"flex flex-row"}>
+                <Image src="/Gibraltar.svg" width={75} height={75} className={"ml-10 cursor-pointer block md:hidden "}></Image>
                 
-                    <h1 className={NavStyle.brandName}>Gibraltr</h1>
-                </div>
-                <div className={NavStyle.hamburgerMenu}>
-                    <HamburgerMenu></HamburgerMenu>
-                </div>
-                <ul className={NavStyle.navItems}>
-                    {Array.from(navItems.entries()).map(([title, details], index) => (
-                        <Link className={"mx-4 hover:border-b-4 hover:border-b-stone-700 cursor-pointer hover:border-solid border-l-0 border-t-0 border-r-0 text-stone-700 hidden md:lg:xl:2xl:block"} href={details.link}>{title}</Link>
-                    ))}
-                </ul>
+                <Image src="/Gibraltar.svg" width={90} height={90} className={"ml-24 cursor-pointer hidden md:block"}></Image>
+            
+                <h1 className={"text-stone-700 font-thin pt-6 md:lg:xl:2xl:mb-0 md:lg:xl:2xl:pt-7 ml-1.5 text-3xl md:lg:xl:2xl:text-4xl"}>Gibraltr</h1>
             </div>
+            <div className={"block md:hidden"}>
+                <HamburgerMenu></HamburgerMenu>
+            </div>
+            <ul className={"w-1/2 list-none mt-10 flex flex-row text-right justify-end max-md:invisible md:visible"}>
+                {Array.from(navItems.entries()).map(([title, details], index) => (
+                    <Link key={index} className={"mx-4 hover:border-b-4 hover:border-b-stone-700 cursor-pointer hover:border-solid border-l-0 border-t-0 border-r-0 text-stone-700 hidden md:block"} href={details.link}>{title}</Link>
+                ))}
+            </ul>
         </div>
     );
 }
