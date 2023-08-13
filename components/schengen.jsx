3170a8b6b570fd00b230490b13743e3ac2a30740
tachorzy <tachorzyad@gmail.com>
2023-08-13 14:50:43 -0500

    Added inline styling to Schengen.jsx

--- components/schengen.jsx
+++ components/schengen.jsx
@@ -10,15 +10,13 @@ const supremeMedium = localFont({
 const Schengen = () => {
 
     return(
-        <div className={styles.visaInfoBox}>
-            <span className={supremeMedium.className}>
-                <span className={styles.infoBoxTitleContainer}>
-                    <p className={styles.visaInfoBoxTitle}>Schengen Zone</p>
-                    <Image src='/EU.svg' width={35} height={35} className={styles.infoBoxImage}></Image>
-                </span>
-
-                <p className={styles.visaInfoBoxText}> Freedom of movement applies across Schengen Zone. Stay up to 90 days in a 180-day period.</p>
+        <div className={supremeMedium.className + " w-96 h-40 text-stone-700 border-2 border-inherit border-solid rounded-xl p-4 pt-5 border-stone-300 bg-stone-200"}>
+            <span className={"grid grid-cols-3"}>
+                <p className={"text-lg font-bold col-span-2 mb-2.5 align-middle"}>Schengen Zone</p>
+                <Image src='/EU.svg' width={35} height={35} className={"ml-16"}></Image>
             </span>
+
+            <p className={"leading-relaxed"}> Freedom of movement applies across Schengen Zone. Stay up to 90 days in a 180-day period.</p>
         </div>  
     );
 }
