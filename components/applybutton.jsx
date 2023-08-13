7648732d1ffc73082a7830d5ca983b631749bec0
tachorzy <tachorzyad@gmail.com>
2023-08-13 13:33:35 -0500

    Removed stylesheet from ApplyButton

--- components/applybutton.jsx
+++ components/applybutton.jsx
@@ -12,7 +12,7 @@ const ApplyButton = ({title}) => {
             <button className={"w-96 h-16 bg-stone-600 hover:bg-stone-700 text-stone-200 font-semibold text-xl rounded-xl ring-stone-600 border-none cursor-pointer"}>
                 <div className={supremeMedium.className + " flex flex-row justify-center gap-x-4"}>
                     <p>Apply for an e-Visa!</p>
-                        <Image src="/ApplyButtonLink.svg" width={20} height={20} className={DestinationStyles.buttonLinkIcon}></Image>
+                        <Image src="/ApplyButtonLink.svg" width={20} height={20} className={"mt-1"}></Image>
                 </div>
             </button>  
     );
