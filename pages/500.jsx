470c1e087fa6f3e3ec4333eea1b2f09ee1ba5a73
tachorzy <tachorzyad@gmail.com>
2023-08-13 21:22:29 -0500

    Updated import statements in 500.jsx

--- pages/500.jsx
+++ pages/500.jsx
@@ -1,17 +1,10 @@
 import Head from 'next/head'
-import Image from 'next/image'
-import NavBar from '../components/navbar'
-import SearchBar from '../components/searchbar'
-import HomePageInfo from '../components/homepageInfo'
-import InfoCard from '../components/infocard'
-import Footer from '../components/footer'
+import NavBar from '../components/NavBar'
+import Footer from '../components/Footer'
 import { Combo, Roboto, Rubik } from '@next/font/google'
 import localFont from '@next/font/local'
 import styles from '../styles/Home.module.css'
-import AboutStyles from '../styles/AboutUsStyle.module.css'
 import ErrorStyles from '../styles/ErrorStyle.module.css'
-import "animate.css"
-
 
 const roboto = Roboto({ 
   subsets: ['latin'], 
