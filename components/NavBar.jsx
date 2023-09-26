d21a4c59cec612a955d606804817a624ca228b12
tachorzy <tachorzyad@gmail.com>
2023-09-26 01:58:38 -0500

    Added conditional rendering for dark and light pages to NavBar

--- components/NavBar.jsx
+++ components/NavBar.jsx
@@ -15,11 +15,7 @@ const navItems = new Map([
 ])
 
 const NavBar = ({lightingMode}) => {
-    
-    if(lightingMode == "light")
-        
-
-
+            
     return(
         <div className={albula.className + " absolute top-0 align-baseline h-20 w-screen space-x-12 md:space-x-96 flex flex-row  z-50"}>
             <div className={"flex flex-row relative "}>
@@ -28,14 +24,14 @@ const NavBar = ({lightingMode}) => {
                     
                     <Image src="/Gibraltar.svg" width={90} height={90} className={"select-none ml-6 fixed cursor-pointer hidden md:block"} alt="Gibraltr logo"></Image>
                 </Link>
-                <h1 className={`${lightMode == "light" ? "text-stone-700" : "text-stone-300"} font-thin absoltue pt-6 md:mb-0 md:pt-7 ml-[97%] md:ml-32 text-3xl md:text-4xl`}>Gibraltr</h1>
+                <h1 className={`${lightingMode == "dark" ? "text-stone-300" : "text-stone-700"} font-thin absoltue pt-6 md:mb-0 md:pt-7 ml-[97%] md:ml-32 text-3xl md:text-4xl`}>Gibraltr</h1>
             </div>
             <div className={"block md:hidden"}>
                 <HamburgerMenu></HamburgerMenu>
             </div>
             <ul className={"w-1/2 list-none mt-10 pr-10 md:pr-24 flex flex-row text-right justify-end max-md:invisible md:visible"}>
                 {Array.from(navItems.entries()).map(([title, details], index) => (
-                    <Link key={index} className={"mx-4 hover:border-b-4 hover:border-b-stone-700 cursor-pointer hover:border-solid border-l-0 border-t-0 border-r-0 text-stone-700 hidden md:block"} href={details.link}>{title}</Link>
+                    <Link key={index} className={`${lightingMode == "dark" ? "text-stone-300" : "text-stone-700"} mx-4 hover:border-b-4 hover:border-b-stone-700 cursor-pointer hover:border-solid border-l-0 border-t-0 border-r-0 hidden md:block`} href={details.link}>{title}</Link>
                 ))}
             </ul>
         </div>
