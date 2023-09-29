35d801df44706ea326e9c79180bdd2246a5b1b5e
tachorzy <tachorzyad@gmail.com>
2023-09-29 18:54:15 -0500

    Adjusted width and height in VisaOnArrival

--- components/VisaOnArrival.jsx
+++ components/VisaOnArrival.jsx
@@ -10,7 +10,7 @@ const supremeMedium = localFont({
 const VisaOnArrival = () => {
 
     return(
-        <div className={supremeMedium.className + " w-96 h-40 text-stone-700 border-2 border-inherit border-solid rounded-xl p-4 pt-5 border-stone-300 bg-stone-200"}>
+        <div className={supremeMedium.className + " w-72 h-56 md:w-96 md:h-40 text-stone-700 border-2 border-inherit border-solid rounded-xl p-4 pt-5 border-stone-300 bg-stone-200"}>
             <p className={"text-lg font-bold col-span-2 mb-2.5 align-middle"}>Visa-on-arrival</p>
             <p className={"leading-relaxed"}>{"Before going through customs, you'll have to fill out a form and possibly pay an entry-fee at the airport."}</p>
         </div>
