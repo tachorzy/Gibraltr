5b268713bb5b94bec3e59616d68b51db378511bf
tachorzy <tachorzyad@gmail.com>
2023-09-29 18:53:56 -0500

    Adjusted width and height in Visa

--- components/Visa.jsx
+++ components/Visa.jsx
@@ -10,7 +10,7 @@ const supremeMedium = localFont({
 const Visa = () => {
 
     return(
-        <div className={supremeMedium.className + " w-96 h-40 text-stone-700 border-2 border-inherit border-solid rounded-xl p-4 pt-5 border-stone-300 bg-stone-200"}>
+        <div className={supremeMedium.className + " w-72 h-56 md:w-96 md:h-40  text-stone-700 border-2 border-inherit border-solid rounded-xl p-4 pt-5 border-stone-300 bg-stone-200"}>
             <p className={"text-lg font-bold col-span-2 mb-2.5 align-middle"}>Visa Required</p>
             <p className={"leading-relaxed"}>{"You must apply for a visa prior to entry. This likely means you'll have to file documents, pay a fee, and visiting an embassy."}</p>
         </div>
