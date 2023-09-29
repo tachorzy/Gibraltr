6a553554f2e5056a986c72879c3c7e5238af6bc9
tachorzy <tachorzyad@gmail.com>
2023-09-29 18:55:45 -0500

    Adjusted width and height in IraqiKurdistanRegion

--- components/IraqiKurdistanRegion.jsx
+++ components/IraqiKurdistanRegion.jsx
@@ -10,7 +10,7 @@ const supremeMedium = localFont({
 const IraqiKurdistanRegion = () => {
 
     return(
-        <div className={supremeMedium.className + " w-96 h-40 text-stone-700 border-2 border-inherit border-solid rounded-xl p-4 pt-5 border-stone-300 bg-stone-200"}>
+        <div className={supremeMedium.className + " w-72 h-56 md:w-96 md:h-40 text-stone-700 border-2 border-inherit border-solid rounded-xl p-4 pt-5 border-stone-300 bg-stone-200"}>
             <span className={"grid grid-cols-3"}>
                 <p className={"text-lg font-bold col-span-2 mb-2.5 align-middle"}>Iraqi Kurdistan Region</p>
                 <Image src='/iraqborder.svg' width={35} height={35} className={"ml-16"}></Image>
