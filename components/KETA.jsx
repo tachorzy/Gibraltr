475b651b0b3e8ca6fabebd2d4d7a9e759583c41a
tachorzy <tachorzyad@gmail.com>
2023-09-29 19:10:07 -0500

    Changed text size and dimensions of KETA

--- components/KETA.jsx
+++ components/KETA.jsx
@@ -10,14 +10,14 @@ const supremeMedium = localFont({
 const KETA = () => {
 
     return(
-        <div className={supremeMedium.className + " w-72 h-56 md:w-96 md:h-40 text-stone-700 border-2 border-inherit border-solid rounded-xl p-4 pt-5 border-stone-300 bg-stone-200"}>
+        <div className={supremeMedium.className + " w-72 md:w-96 h-40 text-stone-700 border-2 border-inherit border-solid rounded-xl p-4 pt-5 border-stone-300 bg-stone-200"}>
             <span className={"grid grid-cols-3"}>
-                <p className={"text-lg font-bold col-span-2 mb-2.5 align-middle"}>K-ETA</p>
+                <p className={"md:text-lg font-bold col-span-2 mb-2.5 align-middle"}>K-ETA</p>
                 {/* <p className={styles.visaInfoBoxTitleSmall}>{"(Korea Electronic Travel Authorization)"}</p> */}
                 <Image src='/taeguk.svg' width={35} height={35} className={"ml-16"}></Image>
             </span>
 
-            <p className={"leading-relaxed"}>The K-ETA is a travel authorization that visa-free visitors to South Korea must receive via an online application.</p>
+            <p className={"max-md:text-sm leading-relaxed"}>The K-ETA is a travel authorization that visa-free visitors to South Korea must receive via an online application.</p>
         </div>  
     );
 }
