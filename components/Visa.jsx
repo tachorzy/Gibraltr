3dd1088a44a768308fa11147b0e2c8542b718e19
tachorzy <tachorzyad@gmail.com>
2023-09-29 19:07:24 -0500

    Changed text size and dimensions of Visa

--- components/Visa.jsx
+++ components/Visa.jsx
@@ -10,9 +10,9 @@ const supremeMedium = localFont({
 const Visa = () => {
 
     return(
-        <div className={supremeMedium.className + " w-72 h-56 md:w-96 md:h-40  text-stone-700 border-2 border-inherit border-solid rounded-xl p-4 pt-5 border-stone-300 bg-stone-200"}>
-            <p className={"text-lg font-bold col-span-2 mb-2.5 align-middle"}>Visa Required</p>
-            <p className={"leading-relaxed"}>{"You must apply for a visa prior to entry. This likely means you'll have to file documents, pay a fee, and visiting an embassy."}</p>
+        <div className={supremeMedium.className + " w-72 md:w-96 h-40  text-stone-700 border-2 border-inherit border-solid rounded-xl p-4 pt-5 border-stone-300 bg-stone-200"}>
+            <p className={"md:text-lg font-bold col-span-2 mb-2.5 align-middle"}>Visa Required</p>
+            <p className={"max-md:text-sm leading-relaxed"}>{"You must apply for a visa prior to entry. This likely means you'll have to file documents, pay a fee, and visiting an embassy."}</p>
         </div>
     );
 }
