0cddb7788efcfa87eb52616f292a83cfff44d267
tachorzy <tachorzyad@gmail.com>
2023-09-29 19:16:36 -0500

    Adjusted text size and dimensions of ElectronicVisa

--- components/ApplyButton.jsx
+++ components/ApplyButton.jsx
@@ -9,8 +9,8 @@ const supremeMedium = localFont({
 const ApplyButton = ({title}) => {
     let evisa;
     return(
-            <button className={"w-96 h-16 bg-stone-600 hover:bg-stone-700 text-stone-200 font-semibold text-xl rounded-xl ring-stone-600 border-none cursor-pointer"}>
-                <div className={supremeMedium.className + " flex flex-row justify-center gap-x-4"}>
+            <button className={"w-72 md:w-96 h-12 md:h-16 bg-stone-600 hover:bg-stone-700 text-stone-200 font-semibold text-xl rounded-xl ring-stone-600 border-none cursor-pointer"}>
+                <div className={supremeMedium.className + " max-md:text-sm flex flex-row justify-center gap-x-4"}>
                     <p>Apply for an e-Visa!</p>
                         <Image src="/ApplyButtonLink.svg" width={20} height={20} className={"mt-1"}></Image>
                 </div>
--- components/ElectronicVisa.jsx
+++ components/ElectronicVisa.jsx
@@ -8,9 +8,9 @@ const supremeMedium = localFont({
 const ElectronicVisa = () => {
 
     return(
-        <div className={supremeMedium.className + " w-96 h-40 text-stone-700 border-2 border-inherit border-solid rounded-xl p-4 pt-5 border-stone-300 bg-stone-200"}>
-            <p className={"text-lg font-bold col-span-2 mb-2.5"}>e-Visa</p>
-            <p className={"leading-relaxed"}>Electronic visas are filled out online on an official government website or through a travel agency.</p>
+        <div className={supremeMedium.className + " w-72 md:w-96 h-40 text-stone-700 border-2 border-inherit border-solid rounded-xl p-4 pt-5 border-stone-300 bg-stone-200"}>
+            <p className={"md:text-lg font-bold col-span-2 mb-2.5"}>e-Visa</p>
+            <p className={"max-md:text-sm leading-relaxed"}>Electronic visas are filled out online on an official government website or through a travel agency.</p>
         </div>  
     );
 }
