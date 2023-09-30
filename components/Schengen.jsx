034c5f24506fd9aac10bfa83913ad9007a420717
tachorzy <tachorzyad@gmail.com>
2023-09-29 19:03:51 -0500

    Changed text size and dimensions of Schengen

--- components/Schengen.jsx
+++ components/Schengen.jsx
@@ -10,13 +10,13 @@ const supremeMedium = localFont({
 const Schengen = () => {
 
     return(
-        <div className={supremeMedium.className + " w-96 h-40 text-stone-700 border-2 border-inherit border-solid rounded-xl p-4 pt-5 border-stone-300 bg-stone-200"}>
+        <div className={supremeMedium.className + " w-72 md:w-96 h-40 text-stone-700 border-2 border-inherit border-solid rounded-xl p-4 pt-5 border-stone-300 bg-stone-200"}>
             <span className={"grid grid-cols-3"}>
-                <p className={"text-lg font-bold col-span-2 mb-2.5 align-middle"}>Schengen Zone</p>
+                <p className={"md:text-lg font-bold col-span-2 mb-2.5 align-middle"}>Schengen Zone</p>
                 <Image src='/EU.svg' width={35} height={35} className={"ml-16"}></Image>
             </span>
 
-            <p className={"leading-relaxed"}> Freedom of movement applies across Schengen Zone. Stay up to 90 days in a 180-day period.</p>
+            <p className={"max-md:text-sm leading-relaxed"}> Freedom of movement applies across Schengen Zone. Stay up to 90 days in a 180-day period.</p>
         </div>  
     );
 }
