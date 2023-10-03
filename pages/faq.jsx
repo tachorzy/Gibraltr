36f578c7490372107e03241f33558d0903f1c2d1
tachorzy <tachorzyad@gmail.com>
2023-10-02 22:28:45 -0500

    Adjusted margins of FAQ page

--- pages/faq.jsx
+++ pages/faq.jsx
@@ -95,12 +95,12 @@ export default function Home() {
           </div>
           
           <div className={supremeMedium.className + " bg-[url('/Gibraltar.svg')] flex flex-row h-[29rem] w-screen bg-stone-800 bg-no-repeat bg-right bg-cover"}>
-            <h1 className={"text-stone-200 text-7xl ml-24 pt-56"}>Frequently Asked Questions</h1>
+            <h1 className={"text-stone-200 text-7xl md:pl-32 pl-10 pt-48 md:pt-56"}>Frequently Asked Questions</h1>
           </div>
         
           <div className={"flex-col gap-y-4 mt-14 mb-20"}>
             <section className={"mb-3.5"}>
-                <h2 className={"text-stone-700 text-4xl mt-12 ml-16 mb-6 font-semibold underline"}>General FAQ</h2>
+                <h2 className={"text-stone-700 text-4xl mt-12 md:pl-40 pl-10  mb-6 font-semibold underline"}>General FAQ</h2>
                 <GeneralFAQ></GeneralFAQ>
             </section>
 
