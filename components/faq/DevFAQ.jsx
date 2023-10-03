37285acb7ce97a5288d084bfdc72be4d049d2f17
tachorzy <tachorzyad@gmail.com>
2023-10-02 23:37:14 -0500

    Adjusted margins in DevFAQ

--- components/faq/DevFAQ.jsx
+++ components/faq/DevFAQ.jsx
@@ -5,10 +5,10 @@ export default function DevFAQ(){
       {Array.from(devFAQContent.entries()).map(([devFAQQuestions, devFAQParagraph]) => {
         return(
         <details key={devFAQQuestions}>
-          <summary className={"text-stone-700 text-3xl font-semibold mb-3.5 ml-16 cursor-pointer"}>
+          <summary className={"text-stone-700 text-3xl font-semibold mb-3.5 md:pl-40 pl-10 cursor-pointer"}>
           {devFAQQuestions}
           </summary>
-          <div className={"text-stone-700 text-xl ml-24 mb-7 w-1/2 leading-relaxed"}>
+          <div className={"text-stone-700 text-xl md:pl-40 pl-10 mb-7 w-1/2 leading-relaxed"}>
             {devFAQParagraph}
           </div>
         </details>
--- pages/faq.jsx
+++ pages/faq.jsx
@@ -44,7 +44,7 @@ export default function Home() {
             </section>
 
             <section className={"mb-3.5"}>
-              <h2 className={"text-stone-700 text-4xl mt-12 ml-16 mb-6 font-semibold underline"}>Developer FAQ</h2>
+              <h2 className={"text-stone-700 text-4xl mt-12 md:pl-40 pl-10 mb-6 font-semibold underline"}>Developer FAQ</h2>
                 <DevFAQ></DevFAQ>
             </section>
           </div>            
