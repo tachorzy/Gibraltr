c8a6fa106d42fc62a8ff3f3b28afec228c9c9428
tachorzy <tachorzyad@gmail.com>
2023-10-02 22:54:49 -0500

    Extracted DevFAQ from faq.jsx

---
+++ components/faq/DevFAQ.jsx
@@ -0,0 +1,20 @@
+import { devFAQContent } from '../utils/faqdata.jsx'
+
+export default function DevFAQ(){
+    return(<div>
+      {Array.from(devFAQContent.entries()).map(([devFAQQuestions, devFAQParagraph]) => {
+        return(
+        <details key={devFAQQuestions}>
+          <summary className={"text-stone-700 text-3xl font-semibold mb-3.5 ml-16 cursor-pointer"}>
+          {devFAQQuestions}
+          </summary>
+          <div className={"text-stone-700 text-xl ml-24 mb-7 w-1/2 leading-relaxed"}>
+            {devFAQParagraph}
+          </div>
+        </details>
+        )
+      })}
+    </div>)
+  }
+  
+  
--- pages/faq.jsx
+++ pages/faq.jsx
@@ -6,6 +6,7 @@ import localFont from '@next/font/local'
 import { visaFAQContent, generalFAQContent, devFAQContent } from '../utils/faqdata.jsx'
 import GeneralFAQ from '../components/faq/GeneralFAQ.jsx'
 import VisaFAQ from '../components/faq/VisaFAQ.jsx'
+import DevFAQ from '../components/faq/DevFAQ.jsx'
 import "animate.css"
 
 const roboto = Roboto({ 
@@ -22,24 +23,6 @@ const rubik = Rubik({
   subsets: ['latin'],
   weight: '400'
 })
-
-function DevFAQ(){
-  return(<div>
-    {Array.from(devFAQContent.entries()).map(([devFAQQuestions, devFAQParagraph]) => {
-      return(
-      <details key={devFAQQuestions}>
-        <summary className={"text-stone-700 text-3xl font-semibold mb-3.5 ml-16 cursor-pointer"}>
-        {devFAQQuestions}
-        </summary>
-        <div className={"text-stone-700 text-xl ml-24 mb-7 w-1/2 leading-relaxed"}>
-          {devFAQParagraph}
-        </div>
-      </details>
-      )
-    })}
-  </div>)
-}
-
 
 export default function Home() {
   return (
