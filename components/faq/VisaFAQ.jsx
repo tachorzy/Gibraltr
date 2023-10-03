04ad0cad5ffc4ce9e4aa4321e15103ff47576d46
tachorzy <tachorzyad@gmail.com>
2023-10-02 22:38:25 -0500

    Extracted VisaFAQ from faq.jsx

---
+++ components/faq/VisaFAQ.jsx
@@ -0,0 +1,20 @@
+import { visaFAQContent } from '../utils/faqdata.jsx'
+
+export default function VisaFAQ(){
+    return (
+      <div>
+          {Array.from(visaFAQContent.entries()).map(([visaFAQQuestion, visaFAQParagraph]) => {
+            return(
+            <details key={visaFAQQuestion}>
+              <summary className={"text-stone-700 text-3xl font-semibold mb-3.5 ml-16 cursor-pointer"}>
+              {visaFAQQuestion}
+              </summary>
+              <div className={"text-stone-700 text-xl ml-24 mb-7 w-1/2 leading-relaxed"}>
+                {visaFAQParagraph}
+              </div>
+            </details>
+            )
+          })}
+      </div>
+    );
+  }
--- pages/faq.jsx
+++ pages/faq.jsx
@@ -22,25 +22,6 @@ const rubik = Rubik({
   weight: '400'
 })
 
-function VisaFAQ(){
-  return (
-    <div>
-        {Array.from(visaFAQContent.entries()).map(([visaFAQQuestion, visaFAQParagraph]) => {
-          return(
-          <details key={visaFAQQuestion}>
-            <summary className={"text-stone-700 text-3xl font-semibold mb-3.5 ml-16 cursor-pointer"}>
-            {visaFAQQuestion}
-            </summary>
-            <div className={"text-stone-700 text-xl ml-24 mb-7 w-1/2 leading-relaxed"}>
-              {visaFAQParagraph}
-            </div>
-          </details>
-          )
-        })}
-    </div>
-  );
-}
-
 function DevFAQ(){
   return(<div>
     {Array.from(devFAQContent.entries()).map(([devFAQQuestions, devFAQParagraph]) => {
