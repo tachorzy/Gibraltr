bd0aa9eff8371f5171b3906e340aa5dd6a016e5d
tachorzy <tachorzyad@gmail.com>
2023-10-25 22:31:40 -0500

    Using hamburger menu dropdown from personal website

--- components/HamburgerMenu.jsx
+++ components/HamburgerMenu.jsx
@@ -15,12 +15,12 @@ const HamburgerMenu = () => {
 
     return (
         <div>
-            <button className={"absolute top-0 right-0 align-baseline mt-2.5 p-2 rounded-2xl cursor-pointer select-none border-transparent"}onClick = {() => setIsActive(!isActive)}>
+            <button className={"absolute top-0 right-0 align-baseline mt-2.5 mr-4 p-2 rounded-2xl cursor-pointer select-none border-transparent"}onClick = {() => setIsActive(!isActive)}>
                 <Image src="/hamburger-menu-stone-700.svg" width={42} height={42} alt="" className="cursor-pointer"/>
             </button>
 
             { isActive ? (
-                <div className={"w-56 h-32 self-end mr-5 md:lg:xl:2xl:mr-20 bg-stone-200 rounded-2xl fixed right-0 mt-2 shadow-xl"} ref={menuRef}>
+                <div className={"w-56 h-58 self-end mr-12 bg-stone-200 rounded-2xl fixed right-0 mt-2 shadow-xl"} ref={menuRef}>
                     <div className={"flex flex-col text-stone-700 font-bold py-3"}>
                         <div className={"flex flex-row gap-x-4 items-center hover:bg-stone-300 pl-6 py-2 select-none"}>      
                             <Link href="/" className={"w-full flex flex-row gap-x-4 items-center"}>
