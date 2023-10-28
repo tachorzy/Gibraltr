b175fbbcb57fe032a5720850403c2c19e4bb1c6a
tachorzy <tachorzyad@gmail.com>
2023-10-27 19:55:35 -0500

    Redesigned HomePageInfo cards

--- components/HomePageInfo.jsx
+++ components/HomePageInfo.jsx
@@ -14,8 +14,8 @@ const supremeRegular = localFont({
 
 const generalInfoCards = new Map([
     ['Search for tourist visas', { info: 'Know whether or not your passport grants visa-free entry for your destination.', image: '/magnify.svg' }],
-    ['Find what you need', { info: 'Be sure whether visa applications are available online, at arrival or at an embassy.', image: '/visa.svg' }],
-    ['Enter stress free', { info: 'Arrive to immigration with rest assured that you\'ve met your visa requirements.', image: '/passport-control-2.svg' }]
+    ['Find what you need', { info: 'Be sure whether visa applications are available online, at arrival or at an embassy.', image: '/documents.svg' }],
+    ['Enter stress free', { info: 'Arrive to immigration with rest assured that you\'ve met your visa requirements.', image: '/customs-officer.svg' }]
 ])
 
 const visaInfoCards = [['Visa Required', 'You must apply for a visa prior to entry. This likely means you\'ll have to file documents, pay a fee, and visiting an embassy.'], ['Visa-On-Arrival', 'Before going through customs, you\'ll have to fill out a form and possibly pay an entry-fee at the airport.'], ['E-Visa', 'Electronic visas are filled out online on an official government website or through a travel agency.']]
@@ -27,13 +27,13 @@ const HomePageInfo = () => {
         <div className={supremeRegular.className}>
             <h2 className={"text-stone-700 text-xl md:text-3xl font-bold col-span-2 md:ml-0 mr-2.5 mb-1.5 align-middle"}>{"It's always best to know, before you go."}</h2>
             <p className={"text-stone-700 text-lg md:text-xl md:mr-96 mr-5 w-[95%] md:w-[35rem] lg:w-[46rem]"}>{"That's why we're here to guide you through visas for wherever you're headed."}<br/>{"Stay informed of your options of a tourist visa for your destination and how to get them!"}</p>
-            <div className={"flex md:flex-row flex-col md:mt-7 mt-14 mb-24 gap-x-10 md:gap-y-0 gap-y-10"}>
+            <div className={"flex md:flex-row flex-col md:mt-7 mt-14 mb-24 gap-x-4 md:gap-y-0 gap-y-10"}>
                     {Array.from(generalInfoCards.entries()).map(([title, infoCardDetails], index) => (
-                        <div key={index} className={"md:w-56 md:h-36 w-11/12 sm:h-24 flex md:flex-col flex-row text-stone-700 border-4 border-inherit border-dotted rounded-xl p-4 pt-5 max-lg:p-5 max-lg:py-6 border-stone-700"}>
-                            <Image src={infoCardDetails.image} width={34} height={34}></Image>
-                            <div className={"flex flex-col md:ml-0 ml-2 md:text-xs sm:text-base mb-2"}>
+                        <div key={index} className={"relative overflow-hidden md:w-60 md:h-36 bg-stone-200 w-11/12 sm:h-24 flex md:flex-col flex-row text-stone-700 border-[3px] border-inherit border-solid rounded-xl p-4 pt-5 max-lg:p-5 max-lg:py-6 border-stone-300"}>
+                            <Image src={infoCardDetails.image} width={136} height={136} className="absolute ml-[6.5rem] z-0"></Image>
+                            <div className={"flex flex-col md:ml-0 ml-2 md:text-xs sm:text-base mb-2 z-50"}>
                                 <p className={"text-sm font-bold col-span-2 md:ml-0 ml-2 align-middle"}>{title}</p>
-                                <p className={"flex flex-col md:ml-0 ml-2 md:text-xs sm:text-base"}>{infoCardDetails.info}</p>
+                                <p className={"flex flex-col w-[70%] md:ml-0 ml-2 md:text-xs sm:text-base"}>{infoCardDetails.info}</p>
                             </div>
                         </div>
                     ))}
