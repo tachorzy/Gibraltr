7b41c8efd065d84d3800ff116d955280ddd30ca2
tachorzy <tachorzyad@gmail.com>
2023-12-22 19:31:33 -0600

    Added 2xl responsiveness to navbar

--- components/NavBar.jsx
+++ components/NavBar.jsx
@@ -23,16 +23,18 @@ const NavBar = ({lightingMode}) => {
                 <Link href="/">
                     <Image src={lightingMode == "light" ? "/gibraltar-stone-800.svg" : "/gibraltar-stone-300.svg"} width={75} height={75} className={"select-none ml-10 absolute cursor-pointer block md:hidden "} alt="Gibraltr logo"></Image>
                     
-                    <Image src={lightingMode == "light" ? "/gibraltar-stone-800.svg" : "/gibraltar-stone-300.svg"} width={90} height={90} className={"select-none ml-6 fixed cursor-pointer hidden md:block"} alt="Gibraltr logo"></Image>
+                    <Image src={lightingMode == "light" ? "/gibraltar-stone-800.svg" : "/gibraltar-stone-300.svg"} width={90} height={90} className={"select-none ml-6 fixed cursor-pointer hidden md:max-2xl:block"} alt="Gibraltr logo"></Image>
+                    
+                    <Image src={lightingMode == "light" ? "/gibraltar-stone-800.svg" : "/gibraltar-stone-300.svg"} width={105} height={105} className={"select-none ml-6 fixed cursor-pointer hidden 2xl:block"} alt="Gibraltr logo"></Image>
                 </Link>
-                <h1 className={`${lightingMode == "light" ? "text-stone-700" : "text-stone-300"} font-thin absoltue pt-6 md:mb-0 md:pt-7 ml-[97%] md:ml-32 text-3xl md:text-4xl`}>Gibraltr</h1>
+                <h1 className={`${lightingMode == "light" ? "text-stone-700" : "text-stone-300"} font-thin absoltue pt-6 md:mb-0 md:pt-7 2xl:pt-8 ml-[97%] md:ml-32 2xl:ml-36 text-3xl md:text-4xl 2xl:text-5xl`}>Gibraltr</h1>
             </div>
             <div className={"block md:hidden"}>
                 <HamburgerMenu lightingMode={lightingMode}></HamburgerMenu>
             </div>
             <ul className={"w-1/2 list-none mt-10 pr-10 md:pr-24 flex flex-row text-right justify-end max-md:invisible md:visible"}>
                 {Array.from(navItems.entries()).map(([title, details], index) => (
-                    <Link key={index} className={`${lightingMode == "light" ? "text-stone-700 hover:border-b-stone-700 " : "text-stone-300 hover:border-b-stone-300"} mx-4 hover:border-b-4 cursor-pointer hover:border-solid border-l-0 border-t-0 border-r-0 hidden md:block`} href={details.link}>{title}</Link>
+                    <Link key={index} className={`${lightingMode == "light" ? "text-stone-700 hover:border-b-stone-700 " : "text-stone-300 hover:border-b-stone-300"} 2xl:text-xl mx-4 hover:border-b-4 cursor-pointer hover:border-solid border-l-0 border-t-0 border-r-0 hidden md:block`} href={details.link}>{title}</Link>
                 ))}
             </ul>
         </div>
