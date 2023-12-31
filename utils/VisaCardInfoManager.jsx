1816e2d7a12339228ebca7691a9a3b6a5a0f04a9
tachorzy <tachorzyad@gmail.com>
2023-12-31 03:30:20 -0600

    Added conditional artciles (an or a) depending on the nationality's demonym

--- utils/VisaCardInfoManager.jsx
+++ utils/VisaCardInfoManager.jsx
@@ -15,11 +15,12 @@ import { passportDemonyms } from '/utils/demonymdata.js'
 export function displayVisaMessage(passport, destination, requirement) {
 
     const passportISO = isoCodesList[passport.toLowerCase()].toLowerCase()
+    const passportDemonym = passportDemonyms.get(passportISO)
 
     if (requirement !== "visa required" && requirement !== "e-visa") {
        return( 
-            <h3 className={"max-md:w-full w-3/4 text-stone-700 mt-3 md:mt-8 text-sm md:text-3xl justify-between leading-10"}>
-                You don't need a visa for {destination} if you have a(n) <b>{passportDemonyms.get(passportISO)}
+            <h3 className={"max-md:w-full w-[85%] text-stone-700 mt-3 md:mt-8 text-sm md:text-3xl justify-between leading-10"}>
+                You don't need a visa for {destination} if you have {(/^[aeiou]$/i).test(passportDemonym[0]) ? "an" : "a"} <b>{passportDemonym}
             </b> passport</h3>
        )
     }
