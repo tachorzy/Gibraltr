ef23efeaa9ec132b2298962e574d922499301b71
tachorzy <tachorzyad@gmail.com>
2023-12-31 20:39:40 -0600

    Added 2xl responsiveness to VisaRequirementMessage

--- components/destinations/VisaRequirementMessage.jsx
+++ components/destinations/VisaRequirementMessage.jsx
@@ -7,14 +7,14 @@ const VisaRequirementMessage = ({passport, destination, requirement}) => {
 
     if (requirement !== "visa required" && requirement !== "e-visa" && requirement !== "visa on arrival" && requirement !== "no admission") {
        return( 
-            <h3 className={"max-md:w-full w-[85%] text-stone-700 mt-3 md:mt-8 text-sm md:text-3xl justify-between leading-10"}>
+            <h3 className={"max-md:w-full w-[85%] text-stone-700 mt-3 md:mt-8 text-sm md:text-3xl 2xl:text-4xl justify-between leading-10"}>
                 {`You don't need a visa for ${destination} if you have ${(/^[aeiou]$/i).test(passportDemonym[0]) ? "an" : "a"}`} <b>{passportDemonym}
             </b> {`passport`}</h3>
        )
     }
     else if (requirement === "visa required" || requirement === "e-visa" || requirement === "visa on arrival" || requirement === "no admission") {
         return (
-            <h3 className={"max-md:w-full w-[85%] text-stone-700 mt-3 md:mt-8 text-sm md:text-3xl justify-between leading-10"}>
+            <h3 className={"max-md:w-full w-[85%] text-stone-700 mt-3 md:mt-8 text-sm md:text-3xl 2xl:text-4xl justify-between leading-10"}>
                 {`You need a visa for ${destination} if you have ${(/^[aeiou]$/i).test(passportDemonym[0]) ? "an" : "a"}`} <b>{passportDemonym}
             </b> {`passport`}</h3>
         )
