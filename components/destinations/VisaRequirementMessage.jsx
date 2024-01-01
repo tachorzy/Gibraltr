2ec39a75e636ddc5b6f6bee6e98cdc0e3a4a0725
tachorzy <tachorzyad@gmail.com>
2023-12-31 18:28:14 -0600

    Fixed build error on VisaRequirementMessage

--- components/destinations/VisaRequirementMessage.jsx
+++ components/destinations/VisaRequirementMessage.jsx
@@ -8,15 +8,15 @@ const VisaRequirementMessage = ({passport, destination, requirement}) => {
     if (requirement !== "visa required" && requirement !== "e-visa" && requirement !== "visa on arrival" && requirement !== "no admission") {
        return( 
             <h3 className={"max-md:w-full w-[85%] text-stone-700 mt-3 md:mt-8 text-sm md:text-3xl justify-between leading-10"}>
-                You don't need a visa for {destination} if you have {(/^[aeiou]$/i).test(passportDemonym[0]) ? "an" : "a"} <b>{passportDemonym}
-            </b> passport</h3>
+                {`You don't need a visa for ${destination} if you have ${(/^[aeiou]$/i).test(passportDemonym[0]) ? "an" : "a"}`} <b>{passportDemonym}
+            </b> {`passport`}</h3>
        )
     }
     else if (requirement === "visa required" || requirement === "e-visa" || requirement === "visa on arrival" || requirement === "no admission") {
         return (
             <h3 className={"max-md:w-full w-[85%] text-stone-700 mt-3 md:mt-8 text-sm md:text-3xl justify-between leading-10"}>
-                You need a visa for {destination} if you have {(/^[aeiou]$/i).test(passportDemonym[0]) ? "an" : "a"} <b>{passportDemonym}
-            </b> passport</h3>
+                {`You need a visa for ${destination} if you have ${(/^[aeiou]$/i).test(passportDemonym[0]) ? "an" : "a"}`} <b>{passportDemonym}
+            </b> {`passport`}</h3>
         )
     }
 }
